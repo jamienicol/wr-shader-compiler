77a65025a4ac227d0161a608d0ad248d9ee1f96c
Jamie Nicol <jamie@jamienicol.me>
2020-02-17 16:43:58 +0000

    compile optimised shaders

--- build.rs
+++ build.rs
@@ -5,11 +5,12 @@ use std::{
     path::{Path, PathBuf},
 };
 
-fn write_shader_file(shader_file_path: &Path, orig_shaders: Vec<PathBuf>) {
+fn write_shader_file(shader_file_path: &Path, orig_shaders: Vec<PathBuf>, opt_shaders: Vec<PathBuf>) {
     let mut shader_file = File::create(shader_file_path).unwrap();
 
     write!(shader_file, "pub struct Shader {{ pub name: &'static str, pub vert: &'static str, pub frag: &'static str }}\n\n").unwrap();
     write!(shader_file, "lazy_static! {{\n").unwrap();
+
     write!(shader_file, "    pub static ref ORIG_SHADERS: Vec<Shader> = {{\n").unwrap();
     write!(shader_file, "        let mut shaders = Vec::new();\n").unwrap();
     for shader in orig_shaders {
@@ -24,7 +25,23 @@ fn write_shader_file(shader_file_path: &Path, orig_shaders: Vec<PathBuf>) {
             frag_path
         ).unwrap();
     }
+    write!(shader_file, "        shaders\n").unwrap();
+    write!(shader_file, "    }};\n").unwrap();
 
+    write!(shader_file, "    pub static ref OPT_SHADERS: Vec<Shader> = {{\n").unwrap();
+    write!(shader_file, "        let mut shaders = Vec::new();\n").unwrap();
+    for shader in opt_shaders {
+        let name = shader.file_stem().unwrap().to_str().unwrap();
+        let vert_path = canonicalize(&shader).unwrap().as_os_str().to_str().unwrap().to_string();
+        let frag_path = canonicalize(&shader.with_extension("frag")).unwrap().as_os_str().to_str().unwrap().to_string();
+        write!(
+            shader_file,
+            "        shaders.push(Shader{{ name: \"{}\", vert: include_str!(\"{}\"), frag: include_str!(\"{}\")}});\n",
+            name,
+            vert_path,
+            frag_path
+        ).unwrap();
+    }
     write!(shader_file, "        shaders\n").unwrap();
     write!(shader_file, "    }};\n").unwrap();
     write!(shader_file, "}}\n").unwrap();
@@ -34,10 +51,10 @@ fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
 
     let shader_file_path = Path::new(&out_dir).join("shaders.rs");
-    let mut orig_shaders = Vec::new();
 
-    let res_dir = Path::new("res/orig_shaders");
-    for entry in read_dir(res_dir).unwrap() {
+    let mut orig_shaders = Vec::new();
+    let orig_shaders_dir = Path::new("res/orig_shaders");
+    for entry in read_dir(orig_shaders_dir).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
 
@@ -45,8 +62,19 @@ fn main() {
             orig_shaders.push(path.to_owned());
         }
     }
-
     orig_shaders.sort_by(|a, b| a.file_name().cmp(&b.file_name()));
 
-    write_shader_file(&shader_file_path, orig_shaders);
+    let mut opt_shaders = Vec::new();
+    let orig_shaders_dir = Path::new("res/opt_shaders");
+    for entry in read_dir(orig_shaders_dir).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        if entry.file_name().to_str().unwrap().ends_with(".vert") {
+            opt_shaders.push(path.to_owned());
+        }
+    }
+    opt_shaders.sort_by(|a, b| a.file_name().cmp(&b.file_name()));
+
+    write_shader_file(&shader_file_path, orig_shaders, opt_shaders);
 }
--- src/main.rs
+++ src/main.rs
@@ -31,17 +31,66 @@ fn main() {
     println!("Renderer: {}", gl.get_string(gl::RENDERER));
     println!("Version: {}", gl.get_string(gl::VERSION));
 
-    println!("Compiling unoptimised shaders");
-    for shader in shader_source::ORIG_SHADERS.iter() {
+    // println!("Compiling unoptimised shaders");
+    // for shader in shader_source::ORIG_SHADERS.iter() {
+    //     let start_time = Instant::now();
+
+    //     let vert = gl.create_shader(gl::VERTEX_SHADER);
+    //     gl.shader_source(vert, &[shader.vert.as_bytes()]);
+    //     gl.compile_shader(vert);
+
+    //     let frag = gl.create_shader(gl::FRAGMENT_SHADER);
+    //     gl.shader_source(frag, &[shader.frag.as_bytes()]);
+    //     gl.compile_shader(frag);
+
+    //     let prog = gl.create_program();
+    //     gl.attach_shader(prog, vert);
+    //     gl.attach_shader(prog, frag);
+
+    //     gl.link_program(prog);
+
+    //     let end_time = Instant::now();
+
+    //     let mut link_status = [0];
+    //     unsafe { gl.get_program_iv(prog, gl::LINK_STATUS, &mut link_status); }
+    //     if link_status[0] != 0 {
+    //         println!("Compiling shader {} took {}ms", shader.name, end_time.duration_since(start_time).as_micros() as f32 / 1000.0);
+    //     } else {
+    //         println!("Compiling shader {} failed", shader.name);
+    //     }
+
+    //     gl.detach_shader(prog, vert);
+    //     gl.detach_shader(prog, frag);
+    //     gl.delete_shader(vert);
+    //     gl.delete_shader(frag);
+    //     gl.delete_program(prog);
+    // }
+
+    println!("Compiling optimised shaders");
+    for shader in shader_source::OPT_SHADERS.iter() {
         let start_time = Instant::now();
 
         let vert = gl.create_shader(gl::VERTEX_SHADER);
         gl.shader_source(vert, &[shader.vert.as_bytes()]);
         gl.compile_shader(vert);
 
+        let mut status = [0];
+        unsafe { gl.get_shader_iv(vert, gl::COMPILE_STATUS, &mut status); }
+        if status[0] == 0 {
+            let log = gl.get_shader_info_log(vert);
+            println!("Failed to compile {} vertex shader: {}", shader.name, log);
+        }
+
         let frag = gl.create_shader(gl::FRAGMENT_SHADER);
         gl.shader_source(frag, &[shader.frag.as_bytes()]);
         gl.compile_shader(frag);
+
+        let log = gl.get_shader_info_log(frag);
+        let mut status = [0];
+        unsafe { gl.get_shader_iv(frag, gl::COMPILE_STATUS, &mut status); }
+        if status[0] == 0 {
+            println!("Failed to compile {} fragment shader: {}", shader.name, log);
+        }
 
         let prog = gl.create_program();
         gl.attach_shader(prog, vert);
@@ -56,7 +105,8 @@ fn main() {
         if link_status[0] != 0 {
             println!("Compiling shader {} took {}ms", shader.name, end_time.duration_since(start_time).as_micros() as f32 / 1000.0);
         } else {
-            println!("Compiling shader {} failed", shader.name);
+            let log = gl.get_program_info_log(prog);
+            println!("Compiling shader {} failed: {}", shader.name, log);
         }
 
         gl.detach_shader(prog, vert);
